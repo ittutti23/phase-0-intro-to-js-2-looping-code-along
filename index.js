7268e1890a2dc71dd55c83cb15e8464a81a198e4
Peyton <1209942pl@gmail.com>
2023-08-19 18:38:48 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+  const greetings= []
+    for (let i = 0; i < names.length; i++) {
+ greetings.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+     } return greetings;
+    }
+    
+    function countDown(i) {
+    while (i > 0) {
+        console.log(i)
+        i-=1
+  }
+console.log(i)
+    }
